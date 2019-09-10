4f5db4a55428edfd6e1e83b98a079346d4b004bb
Gusvarov <nazikgta24@gmail.com>
2019-09-10 16:48:44 +0000

    HTML CSS Finished

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// const burgerMenu = document.querySelector('.nav-list');
+// burgerMenu.addEventListener('click', burgerMenuFunction();
+// function burgerMenuFunction() {
+// if (burgerMenu.className === 'nav-list') {
+//     burgerMenu.className += 'responsive';
+// } else {
+//     burgerMenu.className = 'nav-list';
+// }
+// }
