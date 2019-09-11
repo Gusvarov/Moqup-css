bb99da88296470206c633a4ff9837013926974d5
Gusvarov <nazikgta24@gmail.com>
2019-09-11 10:30:00 +0000

    My Code

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// const menu = document.querySelector('#menu');
+
+// menu.addEventListener('click', burgerMenu());
+
+// function burgerMenu() {
+//     if ( menu.className === "menu") {
+
+//     } else {
+
+//     }
+// }
